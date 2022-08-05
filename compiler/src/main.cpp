7934c3c5065c9cbd362183170d845e3a3eddf2eb
Titouan Guionneau <titouan.guionneau@epita.fr>
2022-08-05 15:10:02 +0200

    Added verbose

--- compiler/src/main.cpp
+++ compiler/src/main.cpp
@@ -6,11 +6,30 @@
 
 using namespace std;
 
-void writeInstructionBytes(Instruction* i, ofstream* file){
+
+// Inefficient way to do it
+void verboseInstruction(Token* func, Token* arg1, Token* arg2, int index){
+    uint16_t codes[2] = {
+        arg1 == nullptr ? (uint16_t) 0x0000 : arg1->getCode().u16,
+        arg2 == nullptr ? (uint16_t) 0x0000 : arg2->getCode().u16
+    };
+    string names[3] = {
+        func->getName(),
+        arg1 == nullptr ? "" : arg1->getName(),
+        arg2 == nullptr ? "" : arg2->getName()
+    };
+
+    printf("%04X: %02X %04X %04X ", index, func->getCode().u8,
+            codes[0], codes[1]);
+    printf("=> %s %s %s\n", names[0].c_str(), names[1].c_str(), names[2].c_str());
+}
+
+void writeInstructionBytes(Instruction* i, int index, ofstream* file){
     Token* function = i->getFunction();
     Token* arg1 = nullptr;
     Token* arg2 = nullptr;
     i->getArgs(&arg1, &arg2);
+    verboseInstruction(function, arg1, arg2, index);
 
     uint8_t funcCode = function->getCode().u8;
     uint16_t codes[2] = {
@@ -33,8 +52,11 @@ int createOutputFile(string pathOut, vector<Instruction*>* instVect){
         cerr << "Couldn't open or create " << pathOut << "!" << endl;
         return 1;
     }
-    for(Instruction* i : *instVect)
-        writeInstructionBytes(i, &file);
+    int index = 0;
+    for(Instruction* i : *instVect){
+        writeInstructionBytes(i, index, &file);
+        index++;
+    }
     file.close();
     return 0;
 }
